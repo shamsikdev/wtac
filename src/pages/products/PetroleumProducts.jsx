5209e5cc2ec22e35df8d7391ece125dad26544d3
Shamsiddin Abdurahmonov <sh.abdurahmonv@gmail.com>
2025-02-14 15:09:32 +0500

    updated responsive of petroleum_products

--- src/pages/products/PetroleumProducts.jsx
+++ src/pages/products/PetroleumProducts.jsx
@@ -1,132 +1,37 @@
-// // import { ArrowRight, CheckCircle } from "lucide-react";
-// import { BiCheckCircle } from "react-icons/bi";
-// import { BsArrowRight } from "react-icons/bs";
-// export default function Home() {
-//   return (
-//     <div className="w-full max-w-[1100px] mx-auto pt-44">
-//       <h2 className="text-4xl text-center text-customBlue font-extrabold mb-6">
-//         ООО Riders Trading Pro
-//       </h2>
-//       <p className="text-customGray text-xl text-center mb-4">
-//         Надежный поставщик нефтепродуктов с 2009 года
-//       </p>
-//       <p className="mb-4 text-customGray w-4/5 text-xl">
-//         <b>ООО Riders Trading Pro</b> - динамично развивающаяся компания,
-//         работающая на рынке СНГ с 2009 года. Мы специализируемся на оптовых
-//         поставках нефтепродуктов высочайшего качества, включая:
-//       </p>
-//       <ul className="list-disc list-inside text-xl text-customGray mb-4">
-//         <li>Бензин</li>
-//         <li>Дизельное топливо</li>
-//         <li>Индустриальные базовые масла</li>
-//       </ul>
-
-//       <section className="mb-12">
-//         <h3 className="text-2xl text-customBlue font-black mb-10">
-//           Наши преимущества
-//         </h3>
-//         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-//           {[
-//             "Широкий ассортимент: в нашем ассортименте вы найдете все необходимые нефтепродукты для вашего бизнеса",
-//             "Безупречное качество: мы предлагаем только сертифицированную продукцию, прошедшую тщательный контроль качества",
-//             "Выгодные цены: мы предоставляем нашим клиентам конкурентные цены на всю нашу продукцию",
-//             "Индивидуальный подход: мы всегда готовы подобрать оптимальные решения для каждого клиента, учитывая его потребности и бюджет",
-//           ].map((advantage, index) => (
-//             <div
-//               key={index}
-//               className="flex items-start shadow-md w-full p-4 rounded-md bg-white"
-//             >
-//               <p className="text-xl text-customGray">{advantage}</p>
-//             </div>
-//           ))}
-//         </div>
-//       </section>
-
-//       <section className="mb-12">
-//         <h3 className="text-2xl text-customBlue font-black mb-4">
-//           Высокий уровень сервиса
-//         </h3>
-//         <p className="text-customGray text-xl mb-4">
-//           Мы предлагаем нашим клиентам высококлассный сервис, включающий:
-//         </p>
-//         <ul className="list-disc list-inside text-xl text-customGray mb-4">
-//           <li>Профессиональную консультацию</li>
-//           <li>Быструю доставку</li>
-//           <li>Постоянную техническую поддержку</li>
-//         </ul>
-//       </section>
-
-//       <section className="mb-12">
-//         <h3 className="text-2xl text-customBlue font-black mb-4">
-//           Базовые масла для вашего производства
-//         </h3>
-//         <p className="text-customGray text-xl mb-4 w-4/5">
-//           ООО Riders Trading Pro предлагает широкий ассортимент базовых масел,
-//           которые могут быть использованы в вашем производстве.
-//         </p>
-//         <h4 className="text-2xl text-customBlue font-black mb-2">
-//           Наши базовые масла:
-//         </h4>
-//         <ul className="list-disc list-inside text-customGray text-xl">
-//           <li className="mb-2">
-//             Соответствуют всем требованиям и стандартам отрасли
-//           </li>
-//           <li className="mb-2">
-//             Обеспечивают надежную и эффективную работу вашего оборудования
-//           </li>
-//           <li className="mb-2">
-//             Могут быть использованы для получения товарных масел путем
-//             добавления присадок
-//           </li>
-//         </ul>
-//         <h4 className="text-2xl text-customBlue font-black mb-4">
-//           С помощью наших базовых масел вы можете:
-//         </h4>
-//         <ul className="list-disc list-inside text-customGray text-xl mb-10">
-//           <li>Снизить затраты на смазочные материалы</li>
-//           <li>Повысить производительность вашего оборудования</li>
-//           <li>Увеличить срок службы вашего оборудования</li>
-//         </ul>
-//       </section>
-//     </div>
-//   );
-// }
 import { useTranslation } from "react-i18next";
-import { BiCheckCircle } from "react-icons/bi";
-import { BsArrowRight } from "react-icons/bs";
 
 export default function PetroleumProducts() {
   const { t } = useTranslation();
 
   return (
-    <div className="w-full max-w-[1100px] mx-auto pt-44">
-      <h2 className="text-4xl text-center text-customBlue font-extrabold mb-6">
+    <div className="w-full max-w-[1100px] mx-auto sm:pt-44 sm:px-0 pt-32   px-6">
+      <h2 className="text-2xl sm:text-3xl sm:text-center text-customBlue font-extrabold mb-4">
         {t("company.title")}
       </h2>
-      <p className="text-customGray text-xl text-center mb-4">
+      <p className="text-customGray text-base sm:text-lg sm:text-center mb-3">
         {t("company.description")}
       </p>
-      <p className="mb-4 text-customGray w-4/5 text-xl">
+      <p className="mb-3 text-customGray w-full text-base sm:text-lg">
         <b>{t("company.title")}</b> - {t("company.about")}
       </p>
-      <ul className="list-disc list-inside text-xl text-customGray mb-4">
+      <ul className="list-disc list-inside text-base sm:text-lg text-customGray mb-3">
         <li>{t("artp_products.gasoline")}</li>
         <li>{t("artp_products.diesel")}</li>
         <li>{t("artp_products.base_oils")}</li>
       </ul>
 
-      <section className="mb-12">
-        <h3 className="text-2xl text-customBlue font-black mb-10">
+      <section className="mb-10">
+        <h3 className="text-xl sm:text-2xl text-customBlue font-black mb-6">
           {t("artp_advantages.title")}
         </h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {["wide_range", "quality", "prices", "individual_approach"].map(
             (key, index) => (
               <div
                 key={index}
-                className="flex items-start shadow-md w-full p-4 rounded-md bg-white"
+                className="flex items-start shadow-md w-full p-3 rounded-md bg-white"
               >
-                <p className="text-xl text-customGray">
+                <p className="text-base sm:text-lg text-customGray">
                   {t(`artp_advantages.${key}`)}
                 </p>
               </div>
@@ -135,43 +40,43 @@ export default function PetroleumProducts() {
         </div>
       </section>
 
-      <section className="mb-12">
-        <h3 className="text-2xl text-customBlue font-black mb-4">
+      <section className="mb-10">
+        <h3 className="text-xl sm:text-2xl text-customBlue font-black mb-3">
           {t("artp_advantages.service")}
         </h3>
-        <p className="text-customGray text-xl mb-4">
+        <p className="text-customGray text-base sm:text-lg mb-3">
           {t("service.description")}
         </p>
-        <ul className="list-disc list-inside text-xl text-customGray mb-4">
+        <ul className="list-disc list-inside text-base sm:text-lg text-customGray mb-3">
           <li>{t("service.consultation")}</li>
           <li>{t("service.delivery")}</li>
           <li>{t("service.support")}</li>
         </ul>
       </section>
 
-      <section className="mb-12">
-        <h3 className="text-2xl text-customBlue font-black mb-4">
+      <section className="mb-10">
+        <h3 className="text-xl sm:text-2xl text-customBlue font-black mb-3">
           {t("base_oils.title")}
         </h3>
-        <p className="text-customGray text-xl mb-4 w-4/5">
+        <p className="text-customGray text-base sm:text-lg mb-3 w-full">
           {t("base_oils.description")}
         </p>
-        <h4 className="text-2xl text-customBlue font-black mb-2">
+        <h4 className="text-xl sm:text-2xl text-customBlue font-black mb-2">
           {t("base_oils.base_oils_advantages.title")}
         </h4>
-        <ul className="list-disc list-inside text-customGray text-xl">
-          <li className="mb-2">
+        <ul className="list-disc list-inside text-customGray text-base sm:text-lg">
+          <li className="mb-1">
             {t("base_oils.base_oils_advantages.standards")}
           </li>
-          <li className="mb-2">
+          <li className="mb-1">
             {t("base_oils.base_oils_advantages.efficiency")}
           </li>
-          <li className="mb-2">{t("base_oils.base_oils_advantages.usage")}</li>
+          <li className="mb-1">{t("base_oils.base_oils_advantages.usage")}</li>
         </ul>
-        <h4 className="text-2xl text-customBlue font-black mb-4">
+        <h4 className="text-xl sm:text-2xl text-customBlue font-black mb-3">
           {t("base_oils.benefits.title")}
         </h4>
-        <ul className="list-disc list-inside text-customGray text-xl mb-10">
+        <ul className="list-disc list-inside text-customGray text-base sm:text-lg mb-6">
           <li>{t("base_oils.benefits.costs")}</li>
           <li>{t("base_oils.benefits.productivity")}</li>
           <li>{t("base_oils.benefits.lifespan")}</li>
